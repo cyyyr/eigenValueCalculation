cd55e009b6337499ef744eab84bce509d21c5576
cyyyr <kirstar13@yandex.ru>
2021-04-16 02:04:31 +0300

    initial commit

---
+++ Matrix.h
@@ -0,0 +1,647 @@
+//
+// Created by cyr on 26.10.2020.
+//
+#pragma once
+#ifndef ILS_PPP_MATRIX_H
+#define ILS_PPP_MATRIX_H
+
+#define EPS 1e-10
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+template<class T>
+class Matrix {
+public:
+    Matrix<T>(int, int);
+
+    [[maybe_unused]] Matrix<T>(T **, int, int);
+
+    Matrix<T>();
+
+    ~Matrix<T>();
+
+    Matrix<T>(const Matrix<T> &);
+
+    Matrix<T> &operator=(const Matrix<T> &);
+
+    inline T &operator()(int x, int y) { return p[x][y]; }
+
+    inline T &operator()(int x, int y) const { return p[x][y]; }
+
+    Matrix<T> &operator+=(const Matrix<T> &);
+
+    Matrix<T> &operator-=(const Matrix<T> &);
+
+    Matrix<T> &operator*=(const Matrix<T> &);
+
+    Matrix<T> &operator*=(T);
+
+    Matrix<T> &operator/=(T);
+
+    void swapRows(int, int);
+
+    Matrix<T> pushBackRow(const std::vector<T> &);
+
+    Matrix<T> pushBackColumn(const std::vector<T> &);
+
+    static Matrix<T> pushMatrixFromVector(const std::vector<T> &, size_t);
+
+    Matrix<T> transpose();
+
+    static Matrix<T> createIdentity(int);
+
+    // functions on augmented matrices
+    static Matrix<T> augment(Matrix<T>, Matrix<T>);
+
+    Matrix<T> gaussianEliminate();
+
+    static Matrix<T> solve(Matrix<T>, Matrix<T>);
+
+    static Matrix<T> bandSolve(Matrix<T>, Matrix<T>, int);
+
+    Matrix<T> rowReduceFromGaussian();
+
+    Matrix<T> inverse();
+
+    [[nodiscard]] inline int getRows() const;
+
+    [[maybe_unused]] inline void setRows(int rows);
+
+    [[nodiscard]] inline int getCols() const;
+
+    [[maybe_unused]] inline void setCols(int cols);
+
+    template<typename U>
+    friend std::ostream &operator<<(std::ostream &, const Matrix<U> &);
+
+    template<typename U>
+    friend std::istream &operator>>(std::istream &, Matrix<U> &);
+
+private:
+    int rows_{}, cols_{};
+    T **p{};
+
+    void allocSpace();
+};
+
+template<class T>
+Matrix<T> operator+(const Matrix<T> &, const Matrix<T> &);
+
+template<class T>
+Matrix<T> operator-(const Matrix<T> &, const Matrix<T> &);
+
+template<class T>
+Matrix<T> operator*(const Matrix<T> &, const Matrix<T> &);
+
+template<class T>
+Matrix<T> operator*(const Matrix<T> &, T);
+
+template<class T>
+Matrix<T> operator*(T, const Matrix<T> &);
+
+template<class T>
+Matrix<T> operator/(const Matrix<T> &, T);
+
+#endif //ILS_PPP_MATRIX_H
+
+/* PUBLIC MEMBER FUNCTIONS********************************/
+
+template<class T>
+Matrix<T>::Matrix(int rows, int cols) : rows_(rows), cols_(cols) {
+    allocSpace();
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] = (T) 0;
+        }
+    }
+}
+
+template<class T>
+[[maybe_unused]] Matrix<T>::Matrix(T **a, int rows, int cols) : rows_(rows), cols_(cols) {
+    allocSpace();
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] = a[i][j];
+        }
+    }
+}
+
+template<class T>
+Matrix<T>::Matrix() : rows_(1), cols_(1) {
+    allocSpace();
+    p[0][0] = 0;
+}
+
+template<class T>
+Matrix<T>::~Matrix() {
+    for (int i = 0; i < rows_; ++i) {
+        delete[] p[i];
+    }
+    delete[] p;
+}
+
+template<class T>
+Matrix<T>::Matrix(const Matrix &m) : rows_(m.rows_), cols_(m.cols_) {
+    allocSpace();
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] = m.p[i][j];
+        }
+    }
+}
+
+template<class T>
+Matrix<T> &Matrix<T>::operator=(const Matrix &m) {
+    if (this == &m) {
+        return *this;
+    }
+
+    if (rows_ != m.rows_ || cols_ != m.cols_) {
+        for (int i = 0; i < rows_; ++i) {
+            delete[] p[i];
+        }
+        delete[] p;
+
+        rows_ = m.rows_;
+        cols_ = m.cols_;
+        allocSpace();
+    }
+
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] = m.p[i][j];
+        }
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> &Matrix<T>::operator+=(const Matrix &m) {
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] += m.p[i][j];
+        }
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> &Matrix<T>::operator-=(const Matrix &m) {
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] -= m.p[i][j];
+        }
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> &Matrix<T>::operator*=(const Matrix &m) {
+    Matrix temp(rows_, m.cols_);
+    for (int i = 0; i < temp.rows_; ++i) {
+        for (int j = 0; j < temp.cols_; ++j) {
+            for (int k = 0; k < cols_; ++k) {
+                temp.p[i][j] += (p[i][k] * m.p[k][j]);
+            }
+        }
+    }
+    return (*this = temp);
+}
+
+template<class T>
+Matrix<T> &Matrix<T>::operator*=(T num) {
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] *= num;
+        }
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> &Matrix<T>::operator/=(T num) {
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            p[i][j] /= num;
+        }
+    }
+    return *this;
+}
+
+template<class T>
+void Matrix<T>::swapRows(int r1, int r2) {
+    double *temp = p[r1];
+    p[r1] = p[r2];
+    p[r2] = temp;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::transpose() {
+    Matrix matrix(cols_, rows_);
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            matrix.p[j][i] = p[i][j];
+        }
+    }
+    return matrix;
+}
+
+template<class T>
+std::ostream &operator<<(std::ostream &os, const Matrix<T> &m) {
+    for (int i = 0; i < m.rows_; ++i) {
+        os << m.p[i][0];
+        for (int j = 1; j < m.cols_; ++j) {
+            os << " " << m.p[i][j];
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
+template<class T>
+std::istream &operator>>(std::istream &is, Matrix<T> &m) {
+    for (int i = 0; i < m.rows_; ++i) {
+        for (int j = 0; j < m.cols_; ++j) {
+            is >> m.p[i][j];
+        }
+    }
+    return is;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::pushBackRow(const std::vector<T> &row) {
+    if (this->cols_ == 1 && this->rows_ == 1) {
+        cols_ = static_cast<int>(row.size());
+        allocSpace();
+        for (int i = 0; i < this->cols_; ++i) {
+            this->p[rows_ - 1][i] = std::move(row[i]);
+        }
+    } else if (this->cols_ == static_cast<int>(row.size())) {
+        ++rows_;
+        T **pBuf = new T *[rows_ - 1];
+        for (int i = 0; i < rows_ - 1; ++i) {
+            pBuf[i] = new T[cols_];
+        }
+        pBuf = p;
+        allocSpace();
+        for (int j = 0; j < rows_ - 1; ++j) {
+            for (int i = 0; i < this->cols_; ++i) {
+                this->p[j][i] = std::move(pBuf[j][i]);
+            }
+        }
+        for (int i = 0; i < this->cols_; ++i) {
+            this->p[rows_ - 1][i] = std::move(row[i]);
+        }
+        for (int i = 0; i < rows_ - 1; ++i) {
+            delete[] pBuf[i];
+        }
+        delete[] pBuf;
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::pushBackColumn(const std::vector<T> &col) {
+    if (this->cols_ == 1 && this->rows_ == 1) {
+        rows_ = static_cast<int>(col.size());
+        allocSpace();
+        for (int i = 0; i < this->rows_; ++i) {
+            this->p[i][cols_ - 1] = std::move(col[i]);
+        }
+    } else if (this->rows_ == static_cast<int>(col.size())) {
+        ++cols_;
+        T **pBuf = new T *[rows_];
+        for (int i = 0; i < rows_; ++i) {
+            pBuf[i] = new T[cols_ - 1];
+        }
+        pBuf = p;
+        allocSpace();
+        for (int j = 0; j < cols_ - 1; ++j) {
+            for (int i = 0; i < this->rows_; ++i) {
+                this->p[i][j] = std::move(pBuf[i][j]);
+            }
+        }
+        for (int i = 0; i < this->rows_; ++i) {
+            this->p[i][cols_ - 1] = std::move(col[i]);
+        }
+        for (int i = 0; i < rows_; ++i) {
+            delete[] pBuf[i];
+        }
+        delete[] pBuf;
+    }
+    return *this;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::pushMatrixFromVector(const std::vector<T> &inputVector, const size_t size){
+    Matrix<T> outputMatrix;
+    for (size_t i = 0; i < size; ++i) {
+        auto first = inputVector.begin() + i * 6;
+        auto last = inputVector.begin()  + (i + 1) * 6;
+        std::vector<double> tempVector(first, last);
+        outputMatrix.pushBackRow(tempVector);
+    }
+    return outputMatrix;
+}
+/* GETTERS AND SETTERS
+ ********************************/
+
+template<class T>
+inline int Matrix<T>::getRows() const {
+    return rows_;
+}
+
+template<class T>
+[[maybe_unused]] inline void Matrix<T>::setRows(int rows) {
+    rows_ = rows;
+}
+
+template<class T>
+inline int Matrix<T>::getCols() const {
+    return cols_;
+}
+
+template<class T>
+[[maybe_unused]] inline void Matrix<T>::setCols(int cols) {
+    cols_ = cols;
+}
+
+/* STATIC CLASS FUNCTIONS
+ ********************************/
+
+template<class T>
+Matrix<T> Matrix<T>::solve(Matrix<T> A, Matrix<T> b)
+{
+    // Gaussian elimination
+    for (int i = 0; i < A.rows_; ++i) {
+        if (A.p[i][i] == 0) {
+            // pivot 0 - throw error
+            std::cerr << ("Error: the coefficient matrix has 0 as a pivot. Please fix the input and try again.");
+        }
+        for (int j = i + 1; j < A.rows_; ++j) {
+            for (int k = i + 1; k < A.cols_; ++k) {
+                A.p[j][k] -= A.p[i][k] * (A.p[j][i] / A.p[i][i]);
+                if (A.p[j][k] < EPS && A.p[j][k] > -1*EPS)
+                    A.p[j][k] = 0;
+            }
+            b.p[j][0] -= b.p[i][0] * (A.p[j][i] / A.p[i][i]);
+            if (A.p[j][0] < EPS && A.p[j][0] > -1*EPS)
+                A.p[j][0] = 0;
+            A.p[j][i] = 0;
+        }
+    }
+
+    // Back substitution
+    Matrix x(b.rows_, 1);
+    x.p[x.rows_ - 1][0] = b.p[x.rows_ - 1][0] / A.p[x.rows_ - 1][x.rows_ - 1];
+    if (x.p[x.rows_ - 1][0] < EPS && x.p[x.rows_ - 1][0] > -1*EPS)
+        x.p[x.rows_ - 1][0] = 0;
+    for (int i = x.rows_ - 2; i >= 0; --i) {
+        int sum = 0;
+        for (int j = i + 1; j < x.rows_; ++j) {
+            sum += A.p[i][j] * x.p[j][0];
+        }
+        x.p[i][0] = (b.p[i][0] - sum) / A.p[i][i];
+        if (x.p[i][0] < EPS && x.p[i][0] > -1*EPS)
+            x.p[i][0] = 0;
+    }
+
+    return x;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::bandSolve(Matrix<T> A, Matrix<T> b, int k)
+{
+    // optimized Gaussian elimination
+    int bandsBelow = (k - 1) / 2;
+    for (int i = 0; i < A.rows_; ++i) {
+        if (A.p[i][i] == 0) {
+            // pivot 0 - throw exception
+            std::cerr << ("Error: the coefficient matrix has 0 as a pivot. Please fix the input and try again.");
+        }
+        for (int j = i + 1; j < A.rows_ && j <= i + bandsBelow; ++j) {
+            int k = i + 1;
+            while (k < A.cols_ && A.p[j][k]) {
+                A.p[j][k] -= A.p[i][k] * (A.p[j][i] / A.p[i][i]);
+                k++;
+            }
+            b.p[j][0] -= b.p[i][0] * (A.p[j][i] / A.p[i][i]);
+            A.p[j][i] = 0;
+        }
+    }
+
+    // Back substitution
+    Matrix x(b.rows_, 1);
+    x.p[x.rows_ - 1][0] = b.p[x.rows_ - 1][0] / A.p[x.rows_ - 1][x.rows_ - 1];
+    for (int i = x.rows_ - 2; i >= 0; --i) {
+        int sum = 0;
+        for (int j = i + 1; j < x.rows_; ++j) {
+            sum += A.p[i][j] * x.p[j][0];
+        }
+        x.p[i][0] = (b.p[i][0] - sum) / A.p[i][i];
+    }
+
+    return x;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::createIdentity(int size) {
+    Matrix<T> temp(size, size);
+    for (int i = 0; i < temp.rows_; ++i) {
+        for (int j = 0; j < temp.cols_; ++j) {
+            if (i == j) {
+                temp.p[i][j] = 1;
+            } else {
+                temp.p[i][j] = 0;
+            }
+        }
+    }
+    return temp;
+}
+
+// functions on AUGMENTED matrices
+template<class T>
+Matrix<T> Matrix<T>::augment(Matrix A, Matrix B) {
+    Matrix AB(A.rows_, A.cols_ + B.cols_);
+    for (int i = 0; i < AB.rows_; ++i) {
+        for (int j = 0; j < AB.cols_; ++j) {
+            if (j < A.cols_)
+                AB(i, j) = A(i, j);
+            else
+                AB(i, j) = B(i, j - B.cols_);
+        }
+    }
+    return AB;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::gaussianEliminate() {
+    Matrix Ab(*this);
+    int rows = Ab.rows_;
+    int cols = Ab.cols_;
+    int Acols = cols - 1;
+
+    int i = 0; // row tracker
+    int j = 0; // column tracker
+
+    // iterate through the rows
+    while (i < rows) {
+        // find a pivot for the row
+        bool pivot_found = false;
+        while (j < Acols && !pivot_found) {
+            if (Ab(i, j) != 0) { // pivot not equal to 0
+                pivot_found = true;
+            } else { // check for a possible swap
+                int max_row = i;
+                double max_val = 0;
+                for (int k = i + 1; k < rows; ++k) {
+                    double cur_abs = Ab(k, j) >= 0 ? Ab(k, j) : -1 * Ab(k, j);
+                    if (cur_abs > max_val) {
+                        max_row = k;
+                        max_val = cur_abs;
+                    }
+                }
+                if (max_row != i) {
+                    Ab.swapRows(max_row, i);
+                    pivot_found = true;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        // perform elimination as normal if pivot was found
+        if (pivot_found) {
+            for (int t = i + 1; t < rows; ++t) {
+                for (int s = j + 1; s < cols; ++s) {
+                    Ab(t, s) = Ab(t, s) - Ab(i, s) * (Ab(t, j) / Ab(i, j));
+                    if (Ab(t, s) < EPS && Ab(t, s) > -1 * EPS)
+                        Ab(t, s) = 0;
+                }
+                Ab(t, j) = 0;
+            }
+        }
+
+        i++;
+        j++;
+    }
+
+    return Ab;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::rowReduceFromGaussian() {
+    Matrix<T> R(*this);
+    int rows = R.rows_;
+    int cols = R.cols_;
+
+    int i = rows - 1; // row tracker
+    int j = cols - 2; // column tracker
+
+    // iterate through every row
+    while (i >= 0) {
+        // find the pivot column
+        int k = j - 1;
+        while (k >= 0) {
+            if (R(i, k) != 0)
+                j = k;
+            k--;
+        }
+
+        // zero out elements above pivots if pivot not 0
+        if (R(i, j) != 0) {
+
+            for (int t = i - 1; t >= 0; --t) {
+                for (int s = 0; s < cols; ++s) {
+                    if (s != j) {
+                        R(t, s) = R(t, s) - R(i, s) * (R(t, j) / R(i, j));
+                        if (R(t, s) < EPS && R(t, s) > -1 * EPS)
+                            R(t, s) = 0;
+                    }
+                }
+                R(t, j) = 0;
+            }
+
+            // divide row by pivot
+            for (k = j + 1; k < cols; ++k) {
+                R(i, k) = R(i, k) / R(i, j);
+                if (R(i, k) < EPS && R(i, k) > -1 * EPS)
+                    R(i, k) = 0;
+            }
+            R(i, j) = 1;
+
+        }
+
+        i--;
+        j--;
+    }
+
+    return R;
+}
+
+template<class T>
+Matrix<T> Matrix<T>::inverse() {
+    Matrix<T> I = Matrix::createIdentity(rows_);
+    Matrix<T> AI = Matrix::augment(*this, I);
+    Matrix<T> U = AI.gaussianEliminate();
+    Matrix<T> IAInverse = U.rowReduceFromGaussian();
+    Matrix<T> AInverse(rows_, cols_);
+    for (int i = 0; i < AInverse.rows_; ++i) {
+        for (int j = 0; j < AInverse.cols_; ++j) {
+            AInverse(i, j) = IAInverse(i, j + cols_);
+        }
+    }
+    return AInverse;
+}
+
+
+/* PRIVATE HELPER FUNCTIONS
+ ********************************/
+
+template<class T>
+void Matrix<T>::allocSpace() {
+    p = new T *[rows_];
+    for (int i = 0; i < rows_; ++i) {
+        p[i] = new T[cols_];
+    }
+}
+
+/* NON-MEMBER FUNCTIONS
+ ********************************/
+
+template<class T>
+Matrix<T> operator+(const Matrix<T> &m1, const Matrix<T> &m2) {
+    Matrix temp(m1);
+    return (temp += m2);
+}
+
+template<class T>
+Matrix<T> operator-(const Matrix<T> &m1, const Matrix<T> &m2) {
+    Matrix<T> temp(m1);
+    return (temp -= m2);
+}
+
+template<class T>
+Matrix<T> operator*(const Matrix<T> &m1, const Matrix<T> &m2) {
+    Matrix<T> temp(m1);
+    return (temp *= m2);
+}
+
+template<class T>
+Matrix<T> operator*(const Matrix<T> &m, const T &num) {
+    Matrix<T> temp(m);
+    return (temp *= num);
+}
+
+template<class T>
+Matrix<T> operator*(const T &num, const Matrix<T> &m) {
+    return (m * num);
+}
+
+template<class T>
+Matrix<T> operator/(const Matrix<T> &m, const T &num) {
+    Matrix<T> temp(m);
+    return (temp /= num);
+}
---
+++ main.cpp
@@ -0,0 +1,183 @@
+#include "Matrix.h"
+#include <cmath>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+Matrix<double> get_matrix(int s) {
+    Matrix<double> A(s, s);
+    for (int i = 0; i < s; ++i) {
+        for (int k = 0; k < s; ++k) {
+            A(i, k) = pow((static_cast<double>(std::abs(i - k) + 1)), -3.0)
+                      + pow(static_cast<double>(i + k - s), 2.0);
+        }
+    }
+    return A;
+}
+
+Matrix<double> givensturn(Matrix<double> A, int p, int q) {
+    double cos_phi = A(p - 1, p) / sqrt(A(p - 1, p) * A(p - 1, p) + A(p - 1, q) * A(p - 1, q));
+    double sin_phi = A(p - 1, q) / sqrt(A(p - 1, p) * A(p - 1, p) + A(p - 1, q) * A(p - 1, q));
+    Matrix<double> O(A.getRows(), A.getCols());
+    for (int i = 0; i < A.getCols(); ++i) {
+        O(i, i) = 1.0;
+    }
+    O(p, p) = cos_phi;
+    O(q, q) = cos_phi;
+    O(q, p) = sin_phi;
+    O(p, q) = -sin_phi;
+    return O.transpose() * A * O;
+}
+
+Matrix<double> tridiag_with_givens(Matrix<double> A) {
+    for (int i = 1; i < A.getCols() - 1; ++i) {
+        for (int j = i + 1; j < A.getCols(); ++j) {
+            A = givensturn(A, i, j);
+        }
+    }
+
+    return A;
+}
+
+double powermethod(const Matrix<double> &A) {
+    std::vector<double> x_0(A.getCols(), 1.0);
+    double eps{1e-16};
+    std::vector<double> x_n = x_0;
+    double lambda{0.0};
+    double old_lambda{1.0};
+
+//    while (std::fabs(1.0 - lambda / old_lambda) > eps) {
+//
+//        old_lambda = lambda;
+//        x_np1 = A * x_n;
+//        lambda = x_np1[0];
+//        x_n = x_np1 / lambda;
+//    }
+    return lambda;
+}
+
+double inverse_powermethod(const Matrix<double> &A) {
+    std::vector<double> x_0(A.getCols(), 1.0);
+    double eps{1e-16};
+    std::vector<double> x_n = x_0;
+    double lambda{0.0};
+    double old_lambda{1.0};
+
+//    while (std::fabs(1.0 - lambda / old_lambda) > eps) {
+//
+//        old_lambda = lambda;
+//        x_np1 = A / x_n;
+//        lambda = x_np1[0];
+//        x_n = x_np1 / lambda;
+//    }
+
+    return 1.0 / lambda;
+
+}
+
+double inverse_powermethod_with_varbias(Matrix<double> A, double t_0, std::vector<double> x_0) {
+    double eps{1e-16};
+    bool rethist{false};
+
+    std::vector<double> x_n = x_0;
+    double old_t{t_0 + 1.0};
+    double t = t_0;
+    int count = 0;
+
+    std::vector<double> t_history, c_history;
+    t_history.push_back(t_0);
+    c_history.push_back(0);
+
+    while (std::fabs(static_cast<double>(t) / static_cast<double>(old_t) - 1.0) > eps) {
+
+        old_t = t;
+        //создаём диагональную матрицу размером A и покомпонентно умножаем на t
+        Matrix<double> t_matrix(A.getRows(), A.getCols());
+        for (int i = 0; i < A.getCols(); ++i) {
+            t_matrix(i, i) = t;
+        }
+//        A -= t_matrix;
+//        A.pushBackColumn(x_n);
+//        Matrix<double> x_np1 = A.gaussianEliminate();
+//        double lambda = x_np1[0];
+//        t += 1 / lambda;
+//        x_n = x_np1 / lambda;
+        count += 1;
+
+        t_history.push_back(t);
+        c_history.push_back(count);
+    }
+
+    if (rethist) {
+        puts("Implement printing history?");
+        return t;
+    } else
+        return t;
+}
+
+std::vector<double> initial_interval_net(const Matrix<double> &A) {
+    int s{A.getCols()};
+    std::vector<double> x(A.getCols(), 1.0);
+    double eps{1e-16};
+
+    double delta{0.0}; //opnorm(A, 2)
+    double lambda_max{powermethod(A)};
+    double lambda_min{inverse_powermethod(A)};
+    double a{lambda_min - delta};
+    double b{lambda_max + delta};
+
+    double step{(b - a) / (5 * s)};
+    std::vector<double> net;
+    for (int i = 0; i < 5 * s + 2; ++i)
+        net.push_back(a + step * (i - 1));
+    return net;
+
+}
+
+double inverse_powermethod_modified(Matrix<double> A, double t) {
+    std::vector<double> x_0(A.getCols(), 1.0);
+    double eps{1e-16};
+    bool rethist{false};
+    int s = A.getCols();
+    std::vector<double> x_n{x_0};
+    double lambda{0.0};
+//    for (int i = 0; i < 10; ++i) {
+//        x_np1 = (A .- t .* Diagonal(ones(Float64, s))) \ x_n;
+//        lambda = x_np1[0];
+//        x_n = x_np1 / lambda;
+//
+//    }
+    double t_0 = t;
+    lambda = inverse_powermethod_with_varbias(A, t_0, x_n);
+
+    return lambda;
+}
+
+std::vector<double> find_them_all(const Matrix<double> &A) {
+    std::vector<double> x_0(A.getCols(), 1.0);
+    double eps{1e-16};
+    bool rethist{false};
+    int s = A.getCols();
+    std::vector<double> net = initial_interval_net(A);
+    std::vector<double> found;
+    for (double i : net)
+        found.push_back(inverse_powermethod_modified(A, i));
+    if (rethist)
+        return found;
+    else {
+        std::sort(found.begin(), found.end());
+        return found;
+    }
+}
+
+int main() {
+    Matrix<double> A = get_matrix(3);
+    std::vector<double> v{1.0, 2.0, 1.0};
+    Matrix<double> b;
+    b.pushBackColumn(v);
+    std::cout << std::fixed << A << '\n';
+    std::cout << std::fixed << b << '\n';
+    Matrix C = Matrix<double>::solve(A, b);
+    std::cout << std::fixed << C << '\n';
+//    std::cout << std::fixed << tridiag_with_givens(A) << '\n';
+}
